ba1954fece9ec767d69a3521d12cba53d9f54a3f
James Hetherington <jamespjh@gmail.com>
2015-07-31 19:44:27 +0100

    Fix test

--- poker_adventures/tests/functional/test_room.py
+++ poker_adventures/tests/functional/test_room.py
@@ -3,5 +3,5 @@ from poker_adventures.tests import *
 class TestRoomController(TestController):
 
     def test_index(self):
-        response = self.app.get(url(controller='room', action='index'))
+        response = self.app.get(url(controller='room', room='monaco', action='index'))
         # Test response...
